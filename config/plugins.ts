a1187a56c268fa17d00a3989d8b3ce80cc36b9ff
kav-pav <kava92@gmail.com>
2023-06-23 01:46:21 +0200

    configure plugin graphql

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,5 +14,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  //
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
   // ...
 });
