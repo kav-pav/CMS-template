aedd3458226991eea9aa10e8806cc67059eb9fec
kav-pav <kava92@gmail.com>
2023-06-23 02:09:44 +0200

    enable playgroud

--- config/plugins.ts
+++ config/plugins.ts
@@ -20,7 +20,7 @@ module.exports = ({ env }) => ({
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
